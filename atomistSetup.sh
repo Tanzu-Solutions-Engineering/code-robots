0282a229597ca5046a7f338af0e7ee0714337315
James Williams <jwilliams@pivotal.io>
2019-02-16 18:26:19 -0500

    update to spring boot 2.1.3

--- atomistSetup.sh
+++ atomistSetup.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 SDM_HOME=/Users/jwilliams/atomist/projects/jwilliams-pivotal/code-robots
+npm install -g @atomist/cli
+atomist create sdm
 
 cp -rp ./@atomist ${SDM_HOME}/node_modules
+
+atomist start --local
