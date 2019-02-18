a4f14e9082d38082318a6b1608cd88980041fffc
James Williams <jwilliams@pivotal.io>
2019-02-18 15:43:42 -0500

    cleanup

--- web-gui/src/config.js
+++ web-gui/src/config.js
@@ -1,7 +1,7 @@
 const config = {
     SNYK_DASHBOARD_URL: "https://app.snyk.io/org/pivotal-demo/",
     PCF_APPS_MGR_URL: "https://console.run.pivotal.io/",
-    FAKE_BANK_BFF_URL: "http://jwilliams123-web-gateway.cfapps.io"
+    FAKE_BANK_BFF_URL: "http://cbots-web-gateway.cfapps.io"
 };
 
 export default {
