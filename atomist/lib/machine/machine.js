7be2118be9a666881b73f9063fec3b162b8e8afe
James Williams <jwilliams@pivotal.io>
2019-02-28 16:29:14 -0500

    added vulnerabilities

--- atomist/lib/machine/machine.js
+++ atomist/lib/machine/machine.js
@@ -14,6 +14,17 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     return new (P || (P = Promise))(function (resolve, reject) {
         function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
@@ -22,29 +33,57 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+var _this = this;
 Object.defineProperty(exports, "__esModule", { value: true });
-const automation_client_1 = require("@atomist/automation-client");
-const sdm_1 = require("@atomist/sdm");
-const sdm_core_1 = require("@atomist/sdm-core");
-const sdm_pack_build_1 = require("@atomist/sdm-pack-build");
-const sdm_pack_issue_1 = require("@atomist/sdm-pack-issue");
-const sdm_pack_sloc_1 = require("@atomist/sdm-pack-sloc");
-const sdm_pack_spring_1 = require("@atomist/sdm-pack-spring");
-const axios_1 = require("axios");
+var automation_client_1 = require("@atomist/automation-client");
+var sdm_1 = require("@atomist/sdm");
+var sdm_core_1 = require("@atomist/sdm-core");
+var sdm_pack_build_1 = require("@atomist/sdm-pack-build");
+var sdm_pack_issue_1 = require("@atomist/sdm-pack-issue");
+var sdm_pack_sloc_1 = require("@atomist/sdm-pack-sloc");
+var sdm_pack_spring_1 = require("@atomist/sdm-pack-spring");
+var axios_1 = require("axios");
 function machine(configuration) {
-    const sdm = sdm_core_1.createSoftwareDeliveryMachine({
+    var sdm = sdm_core_1.createSoftwareDeliveryMachine({
         name: "Spring software delivery machine",
-        configuration,
+        configuration: configuration,
     });
-    const autofix = new sdm_1.Autofix();
-    const inspect = new sdm_1.AutoCodeInspection();
-    const checkGoals = sdm_1.goals("checks")
+    var autofix = new sdm_1.Autofix();
+    var inspect = new sdm_1.AutoCodeInspection();
+    var checkGoals = sdm_1.goals("checks")
         .plan(autofix)
         .plan(inspect, new sdm_1.PushImpact()).after(autofix);
-    const buildGoals = sdm_1.goals("build")
-        .plan(new sdm_pack_build_1.Build().with(Object.assign({}, sdm_pack_spring_1.MavenDefaultOptions, { builder: sdm_pack_spring_1.mavenBuilder() })))
+    var buildGoals = sdm_1.goals("build")
+        .plan(new sdm_pack_build_1.Build().with(__assign({}, sdm_pack_spring_1.MavenDefaultOptions, { builder: sdm_pack_spring_1.mavenBuilder() })))
         .after(autofix);
-    const deployGoals = sdm_1.goals("deploy")
+    var deployGoals = sdm_1.goals("deploy")
         .plan(new sdm_pack_spring_1.MavenPerBranchDeployment()).after(buildGoals);
     sdm.withPushRules(sdm_1.onAnyPush().setGoals(checkGoals), sdm_1.whenPushSatisfies(sdm_pack_spring_1.IsMaven).setGoals(buildGoals), sdm_1.whenPushSatisfies(sdm_pack_spring_1.HasSpringBootPom, sdm_pack_spring_1.HasSpringBootApplicationClass, sdm_pack_spring_1.IsMaven).setGoals(deployGoals));
     sdm.addExtensionPacks(sdm_pack_spring_1.springSupport({
@@ -92,18 +131,31 @@ exports.LicenseFilename = "LICENSE";
 exports.AddLicenseFile = {
     name: "License Fix",
     pushTest: sdm_1.not(sdm_1.hasFile(exports.LicenseFilename)),
-    transform: (p) => __awaiter(this, void 0, void 0, function* () {
-        const license = yield axios_1.default.get("https://www.apache.org/licenses/LICENSE-2.0.txt");
-        return p.addFile(exports.LicenseFilename, license.data);
-    }),
+    transform: function (p) { return __awaiter(_this, void 0, void 0, function () {
+        var license;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0: return [4 /*yield*/, axios_1.default.get("https://www.apache.org/licenses/LICENSE-2.0.txt")];
+                case 1:
+                    license = _a.sent();
+                    return [2 /*return*/, p.addFile(exports.LicenseFilename, license.data)];
+            }
+        });
+    }); },
 };
 exports.PushSpringBootUpgrade = {
     name: "PushSpringBootUpgrade",
     description: "Create git branch and push to github",
     intent: "push upgrade",
-    listener: (ci) => __awaiter(this, void 0, void 0, function* () {
-        const result = yield sdm_1.execPromise("git", ['push', 'origin', 'boot-upgrade'], { cwd: "../code-robots" });
-        return ci.addressChannels(result.stdout);
-    }),
+    listener: function (ci) { return __awaiter(_this, void 0, void 0, function () {
+        var result;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0: return [4 /*yield*/, sdm_1.execPromise("git", ['push', 'origin', 'boot-upgrade'], { cwd: "../code-robots" })];
+                case 1:
+                    result = _a.sent();
+                    return [2 /*return*/, ci.addressChannels(result.stdout)];
+            }
+        });
+    }); },
 };
-//# sourceMappingURL=machine.js.map
